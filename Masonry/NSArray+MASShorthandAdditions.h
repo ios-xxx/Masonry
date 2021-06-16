4358b52eb63f4b5f3606331dba6738339d9ee0f3
ios-xxx <529404579@qq.com>
2021-06-16 12:13:11 +0800

    Update NSArray+MASShorthandAdditions.h

--- Masonry/NSArray+MASShorthandAdditions.h
+++ Masonry/NSArray+MASShorthandAdditions.h
@@ -5,7 +5,7 @@
 //  Created by Jonas Budelmann on 22/07/13.
 //  Copyright (c) 2013 Jonas Budelmann. All rights reserved.
 //
-
+#define MAS_SHORTHAND
 #import "NSArray+MASAdditions.h"
 
 #ifdef MAS_SHORTHAND
